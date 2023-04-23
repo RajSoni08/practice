e28a35370f1b6a77b9333a74f975a678968815c7
RajSoni08 <raj.soni@kellton.com>
2023-04-23 12:01:32 +0530

    hiii

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿using Classes;
+public class program{
+    public static void Main(string[] args){
+        var account = new BankAccount("<name>", 1000);
+Console.WriteLine($"Account {account.Number} was created for {account.Owner} with {account.Balance} initial balance.");
+
+    }
+    
+}
+

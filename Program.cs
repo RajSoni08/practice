359dd2c85363977484c65cb3173d0f1ce2f88fe8
RajSoni08 <raj.soni@kellton.com>
2023-04-23 12:31:11 +0530

    hiiii2

--- Program.cs
+++ Program.cs
@@ -1,10 +1,41 @@
-﻿using Classes;
-public class program{
-    public static void Main(string[] args){
-        var account = new BankAccount("<name>", 1000);
-Console.WriteLine($"Account {account.Number} was created for {account.Owner} with {account.Balance} initial balance.");
+﻿using System;
+using Classes;
 
-    }
-    
+public class Program
+{
+    static void Main(string[] args){
+
+var account = new BankAccount("SBI",10000);
+//var account2 = new BankAccount("SBH",10000);
+Console.WriteLine($"Account {account.Number} was created for {account.Owner} with {account.Balance} initial balance.");
+//Console.WriteLine($"Account {account2.Number} was created for {account2.Owner} with {account2.Balance} initial balance.");
+    account.MakeWithdrawal(500, DateTime.Now, "Rent payment");
+Console.WriteLine(account.Balance);
+account.MakeDeposit(1000, DateTime.Now, "Friend paid me back");
+Console.WriteLine(account.Balance);
+Console.WriteLine(account.GetAccountHistory());
+// Test that the initial balances must be positive.
+BankAccount invalidAccount;
+try
+{
+    invalidAccount = new BankAccount("invalid", -55);
+}
+catch (ArgumentOutOfRangeException e)
+{
+    Console.WriteLine("Exception caught creating account with negative balance");
+    Console.WriteLine(e.ToString());
+    return;
+}
+// Test for a negative balance.
+try
+{
+    account.MakeWithdrawal(750, DateTime.Now, "Attempt to overdraw");
+}
+catch (InvalidOperationException e)
+{
+    Console.WriteLine("Exception caught trying to overdraw");
+    Console.WriteLine(e.ToString());
 }
 
+    }
+}
